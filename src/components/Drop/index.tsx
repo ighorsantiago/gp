57c6f8206a73e53c81c69e992a8379221861bd9b
Ighor Santiago <ighor_santiago@hotmail.com>
2025-02-03 19:05:44 -0300

    Limpeza do código 2

--- src/components/Drop/index.tsx
+++ src/components/Drop/index.tsx
@@ -1,5 +1,4 @@
 import Dropdown from "react-bootstrap/Dropdown";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
 import DropdownButton from "react-bootstrap/DropdownButton";
 
 import { states } from "../../utils/states";
